ef5a7943fac89fc8f868b2348858d99bd60be689
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-01-29 07:05:18 +0300

    HW-2

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -3,30 +3,30 @@ import Affairs from './affairs/Affairs'
 import s2 from '../../s1-main/App.module.css'
 
 /*
-* 1 - описать типы AffairPriorityType, AffairType
-* 2 - указать нужный тип для defaultAffairs
-* 3 - дописать типы и логику функции filterAffairs и проверить её тестами
-* 4 - выполнить пункт 3 для функции deleteAffair
-* 5 - указать нужный тип в useState с affairs
-* 6 - дописать тип и логику функции deleteAffairCallback
-* 7 - в файле Affairs.tsx дописать типизацию пропсов
-* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow
-* 9 - в файле Affair.tsx дописать типизацию пропсов
-* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать
-* 11 - в файле Affair.tsx отобразить приходящие данные
+* 1 - описать типы AffairPriorityType, AffairType (done)
+* 2 - указать нужный тип для defaultAffairs (done)
+* 3 - дописать типы и логику функции filterAffairs и проверить её тестами (done)
+* 4 - выполнить пункт 3 для функции deleteAffair (done)
+* 5 - указать нужный тип в useState с affairs (done)
+* 6 - дописать тип и логику функции deleteAffairCallback (done)
+* 7 - в файле Affairs.tsx дописать типизацию пропсов (done)
+* 8 - в файле Affairs.tsx дописать логику функций setAll, setHigh, setMiddle, setLow (done)
+* 9 - в файле Affair.tsx дописать типизацию пропсов (done)
+* 10 - в файле Affair.tsx дописать функции deleteCallback и использовать (done)
+* 11 - в файле Affair.tsx отобразить приходящие данные (done)
 * */
 
 // types
-export type AffairPriorityType = any // need to fix any
+export type AffairPriorityType = 'high' | 'middle' | 'low' // need to fix any
 export type AffairType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: number // need to fix any
+    name: string // need to fix any
     priority: AffairPriorityType
 }
 export type FilterType = 'all' | AffairPriorityType
 
 // constants
-const defaultAffairs: any = [ // need to fix any
+const defaultAffairs: AffairType[] = [ // need to fix any
     {_id: 1, name: 'React', priority: 'high'}, // студенты могут изменить содержимое name и количество элементов в массиве, ...priority не менять!
     {_id: 2, name: 'anime', priority: 'low'},
     {_id: 3, name: 'games', priority: 'low'},
@@ -35,23 +35,29 @@ const defaultAffairs: any = [ // need to fix any
 ]
 
 // pure helper functions
-export const filterAffairs = (affairs: any, filter: any): any => { // need to fix any
-
-
-    return affairs // need to fix
+export const filterAffairs = (affairs: AffairType[], filter: FilterType): AffairType[] => { // need to fix any ?
+    switch (filter) {
+        case "low":
+            return affairs.filter(affairs => affairs.priority === 'low')
+        case "middle":
+            return affairs.filter(affairs => affairs.priority === 'middle')
+        case "high":
+            return affairs.filter(affairs => affairs.priority === 'high')
+        default:
+            return affairs // need to fix
+    }
 }
-export const deleteAffair = (affairs: any, _id: any): any => { // need to fix any
-
-    return affairs // need to fix
+export const deleteAffair = (affairs: AffairType[], _id: number): AffairType[] => { // need to fix any
+    return affairs.filter(affairs => affairs._id !== _id)
 }
 
 function HW2() {
-    const [affairs, setAffairs] = useState<any>(defaultAffairs) // need to fix any
+    const [affairs, setAffairs] = useState<AffairType[]>(defaultAffairs) // need to fix any
     const [filter, setFilter] = useState<FilterType>('all')
 
     const filteredAffairs = filterAffairs(affairs, filter)
-    const deleteAffairCallback = (_id: any) => { // need to fix any
-        // need to fix
+    const deleteAffairCallback = (_id: number) => { // need to fix any
+        setAffairs(affairs.filter(affairs => affairs._id !== _id))
     }
 
     return (

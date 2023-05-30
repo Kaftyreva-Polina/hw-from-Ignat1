4064121e17966c66433dc215ffc2d5090b681f05
KaftyrevaPolina <pkaft.13.8@gmail.com>
2023-05-30 23:37:29 +0300

    HW 08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,18 @@
-import {UserType} from '../HW8'
+import {UserType} from "../HW8"
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+    | { type: "sort"; payload: "up" | "down" }
+    | { type: "check"; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+        case "sort": {
+            return state.map(el => ({...el})).sort((a, b) => (action.payload === "up")
+                ? a.name.localeCompare(b.name)
+                : b.name.localeCompare(a.name))
         }
-        case 'check': {
-
-            return state // need to fix
+        case "check": {
+            return state.filter(el => el.age >= 18).sort((a,b) => a.age - b.age)
         }
         default:
             return state
